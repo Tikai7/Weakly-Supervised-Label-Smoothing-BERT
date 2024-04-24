1672b6e507b228fa6559360038c8cc264ed1bbc7
Tikai7 <halimi.abdelkrim@outlook.fr>
2024-04-24 10:14:15 +0200

    version 0.2 added Bert model

--- model/Bert.py
+++ model/Bert.py
@@ -1,16 +1,16 @@
 import torch.nn as nn
-import transformers
-from transformers.modeling_bert import BertModel
+from transformers import BertModel
 
-class Bert(transformers.modeling_bert.BertPreTrainedModel):
-    def __init__(self, hidden_size=128, output_size=2, dropout_rate=0.5) -> None:
-        super().__init__()
-        self.bert = BertModel()
+class Bert(nn.Module):
+    def __init__(self, model_name="bert-base-uncased", output_size=2, dropout_rate=0.5) -> None:
+        super(Bert,self).__init__()
+        self.bert = BertModel.from_pretrained(model_name)
         self.dropout = nn.Dropout(dropout_rate)
-        self.classifier = nn.Linear(hidden_size, output_size)
+        self.fc = nn.Linear(self.bert.config.hidden_size, output_size)
 
-    def forward(self, inputs):
-        outputs = self.bert(inputs)
-        outputs = self.dropout(outputs)
-        outputs = self.classifier(outputs)
-        return outputs
+    def forward(self, inputs, attention_mask):
+        outputs = self.bert(inputs, attention_mask)
+        pooled_outputs = outputs.pooler_output
+        logits = self.dropout(pooled_outputs)
+        logits = self.fc(logits)
+        return logits
--- model/Loss.py
+++ model/Loss.py
@@ -1,8 +1,5 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-
 class LSLoss(nn.Module):
     def __init__(self, smoothing, distribution) -> None:
         super().__init__()
@@ -12,7 +9,6 @@ class LSLoss(nn.Module):
     def forward(self, y_pred, y_true):
         log_proba = F.log_softmax(y_pred)
         ground_truth_distribution = (1-self.smoothing)*y_true + self.smoothing*self.distribution
-        
 
 class T_LSLoss(nn.Module):
     pass
